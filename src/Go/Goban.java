e45dde65e9ee0ba3235df03b9145c0a3607d1007
justfreezo <pisraelmartins@gmail.com>
2023-11-20 22:11:57 +0100

    Updated Goban constructor

--- src/Go/Goban.java
+++ src/Go/Goban.java
@@ -3,10 +3,15 @@ package Go;
 public class Goban {
     private static final int DEFAULT_SIZE = 19;
     private int size;
-    private Stone[] stones;
+    private Stone[][] stones;
 
     public Goban(){
         this.size = DEFAULT_SIZE;
-        this.stones = new Stone[DEFAULT_SIZE * DEFAULT_SIZE];
+        this.stones = new Stone[DEFAULT_SIZE][DEFAULT_SIZE];
+    }
+
+    public Goban(int n){
+        this.size = n;
+        this.stones = new Stone[n][n];
     }
 }
