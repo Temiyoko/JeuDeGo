50da99c386812d4a2d4aea74efd74c4128a05a1a
Yukiwi <117077762+Temiyoko@users.noreply.github.com>
2023-11-20 18:24:59 +0100

    Coded boardsize

--- src/jeu/JeuDeGo.java
+++ src/jeu/JeuDeGo.java
@@ -3,13 +3,19 @@ package jeu;
 import java.util.Scanner;
 
 public class JeuDeGo {
+    private static final int MINSIZE = 1, MAXSIZE = 19;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         while(sc.hasNextLine()) {
             String saisie = sc.nextLine().trim();
+
             if (saisie.equals("quit")){
                 System.out.println("=");
                 break;
+            }
+            else if (saisie.startsWith("boardsize")) {
+                boardsize(saisie);
             }
             else{
                 System.out.println("? unknown command");
@@ -17,4 +23,29 @@ public class JeuDeGo {
         }
         sc.close();
     }
+
+    private static void boardsize(String input) {
+        try {
+            String[] parts = input.split("\\s+");
+            int nb = Integer.parseInt(parts[1]);
+
+            if(nb < MINSIZE || nb > MAXSIZE){
+                System.out.println("? unacceptable size");
+                return;
+            }
+
+            // The board size is changed.
+            // The board configuration, number of captured stones, and move history become arbitrary
+            System.out.println("=");
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("? boardsize not an integer");
+            return;
+        }
+        catch (NumberFormatException e) {
+            System.out.println("? unknown command");
+            return;
+        }
+    }
+
 }
