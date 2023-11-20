eed45953801f7fea1af3fccfd26500cf47908abf
justfreezo <pisraelmartins@gmail.com>
2023-11-20 16:49:18 +0100

    Command quit

--- src/jeu/JeuDeGo.java
+++ src/jeu/JeuDeGo.java
@@ -1,7 +1,20 @@
 package jeu;
 
+import java.util.Scanner;
+
 public class JeuDeGo {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextLine()) {
+            String saisie = sc.nextLine().trim();
+            if (saisie.equals("quit")){
+                System.out.println("=");
+                break;
+            }
+            else{
+                System.out.println("? unknown command");
+            }
+        }
+        sc.close();
     }
 }
