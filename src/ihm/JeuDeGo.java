2e32454af0942c28a65c0d90c252c0ca92bd274a
justfreezo <pisraelmartins@gmail.com>
2023-12-29 18:51:52 +0100

    Added new methods to the method play

--- src/ihm/JeuDeGo.java
+++ src/ihm/JeuDeGo.java
@@ -13,68 +13,55 @@ public class JeuDeGo {
     private static Map<Player, List<String>> history;
 
     public static void main(String[] args) {
-        initializeGame();
-        Player lastPlayer = whiteP;
+        goban = new Goban();
+        blackP = new Player(Stones.BLACK);
+        whiteP = new Player(Stones.WHITE);
+        history = new HashMap<>(Map.of(blackP, new ArrayList<>(), whiteP, new ArrayList<>()));
+        String id, cmd;
+        String[] arg;
+
         Scanner sc = new Scanner(System.in);
 
-        while (sc.hasNextLine()) {
+        while(sc.hasNextLine()) {
             String input = sc.nextLine().trim();
             String[] arguments = input.split("\\s+");
 
-            String id, cmd;
-            String[] arg;
-
-            if (isInt(arguments[0])) {
+            if(isInt(arguments[0])){
                 id = arguments[0];
                 cmd = arguments[1];
                 arg = Arrays.copyOfRange(arguments, 2, arguments.length);
-            } else {
+            }
+            else{
                 id = "";
                 cmd = arguments[0];
                 arg = Arrays.copyOfRange(arguments, 1, arguments.length);
             }
 
-            switch (cmd) {
-                case "quit":
-                    System.out.println("=" + id);
-                    return;
-                case "boardsize":
-                    boardsize(arg, id);
-                    break;
-                case "showboard":
-                    showBoard(id);
-                    break;
-                case "clear_board":
-                    resetGame(goban.getSize());
-                    System.out.println("=" + id);
-                    break;
-                case "play":
-                    lastPlayer = play(arg, id, lastPlayer);
-                    if (lastPlayer == null) {
-                        return;
-                    }
-                    break;
-                default:
-                    System.out.println("?" + id + " unknown command");
-                    break;
+            if (cmd.equals("quit")){
+                System.out.println("=" + id);
+                break;
+            }
+            else if (cmd.equals("boardsize")) {
+                boardsize(arg, id);
+            }
+            else if (cmd.equals("showboard")){
+                System.out.println("=" + id);
+                System.out.println(goban.show(blackP, whiteP));
+            }
+            else if (cmd.equals("clear_board")) {
+                resetGame(goban.getSize());
+                System.out.println("=" + id);
+            }
+            else if (cmd.equals("play")) {
+                play(arg, id);
+            }
+            else{
+                System.out.println("?" + id +" unknown command");
             }
         }
         sc.close();
     }
-
-    private static void initializeGame() {
-        goban = new Goban();
-        blackP = new Player(Stones.BLACK);
-        whiteP = new Player(Stones.WHITE);
-        history = new HashMap<>(Map.of(blackP, new ArrayList<>(), whiteP, new ArrayList<>()));
-    }
-
-    private static void showBoard(String id) {
-        System.out.println("=" + id);
-        System.out.println(goban.show(blackP, whiteP));
-    }
-
-    private static int[] convert(String s){
+    private static int[] convert(String s){ //A1
         char c = s.toUpperCase().charAt(0);
         if (c == 'I') {
             throw new IllegalArgumentException();
@@ -83,28 +70,18 @@ public class JeuDeGo {
         int ligne = Integer.parseInt(s.substring(1)) - 1;
         return new int[]{col,  ligne};
     }
-    private static Player play(String[] arg, String id, Player lastP) {
+    private static void play(String[] arg, String id) { //play white A1 arreter le code quand prob
         try {
             if (!arg[0].equalsIgnoreCase("white") && !arg[0].equalsIgnoreCase("black")) {
                 throw new IllegalArgumentException();
             }
             Player p = arg[0].equalsIgnoreCase("black") ? blackP : whiteP;
 
-            if (p == lastP) {
-                throw new RuntimeException();
-            }
-
             if (arg[1].equalsIgnoreCase("pass")) {
-                List<String> prev = history.get(p);
-                if(!prev.isEmpty() && prev.get(prev.size() - 1).equalsIgnoreCase("pass")){
-                    System.out.println("=" + id + " resigns");
-                    return null;
-                }
-                prev.add(arg[1]);
+                history.get(p).add(arg[1]);
                 System.out.println("=" + id);
-                return p;
+                return;
             }
-
             int[] coord = convert(arg[1]);
             if (!goban.isInBoard(coord)) {
                 throw new IllegalArgumentException();
@@ -112,22 +89,20 @@ public class JeuDeGo {
             else if (!goban.isPlayable(coord)) {
                 throw new RuntimeException();
             }
+
             Stones color = (p == blackP) ? Stones.BLACK : Stones.WHITE;
 
             history.get(p).add(arg[1]);
-            goban.setStones(coord, color);
-
-            p.setScore(p.getScore() + captureStones(coord));
-
+            goban.setGoban(coord, color);
             System.out.println("=" + id);
-            return p;
+            System.out.println(history);
+
 
         } catch (IllegalArgumentException | ArrayIndexOutOfBoundsException e) {
             System.out.println("?" + id + " invalid color or coordinate");
         } catch (RuntimeException e) {
             System.out.println("?" + id + " illegal move");
         }
-        return lastP;
     }
 
     public static int captureStones(int[] position) {
@@ -137,13 +112,12 @@ public class JeuDeGo {
                 int stoneLiberties = goban.getLiberties(adjPos);
                 if (stoneLiberties == 0) {
                     goban.setStones(adjPos, null);
-                    cpt++;
+                    ++cpt;
                 }
             }
         }
         return cpt;
     }
-
     private static void boardsize(String[] args, String id) {
         try {
             int nb = Integer.parseInt(args[0]);
@@ -167,16 +141,18 @@ public class JeuDeGo {
     private static void resetGame(int nb) {
         assert nb >= goban.getMinSize() && nb <= goban.getMaxSize();
 
-        goban = new Goban(nb);
+        goban = new Goban(nb); // The board size is changed and attributes are reset
+        // The count of stones captured by each player will be reset to an arbitrary state.
         blackP.resetScore();
         whiteP.resetScore();
-        history = new HashMap<>(Map.of(blackP, new ArrayList<>(), whiteP, new ArrayList<>()));
+        // The history of moves made in the game will be reset to an arbitrary state.
+        history = new HashMap<>();
     }
 
     private static boolean isInt(String s){
         try{
-           Integer.parseInt(s);
-           return true;
+            Integer.parseInt(s);
+            return true;
         } catch (NumberFormatException e) {
             return false;
         }
